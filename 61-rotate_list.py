2975312c7116c8d0442f4578f23317004658f89d
Swee <110452211+Sparkysn@users.noreply.github.com>
2026-05-06 00:45:06 +0800

    add in optimise solution

--- 61-rotate_list.py
+++ 61-rotate_list.py
@@ -71,6 +71,40 @@ class Solution:
                         curr.val = temp_val2
                         curr = curr.next
             
-
-
         return curr
+
+# time: O(n)
+# space: O(1)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        # Find length and tail
+        length = 1
+        tail = head
+
+        while tail.next:
+            tail = tail.next
+            length += 1
+
+        k = k % length
+        if k == 0:
+            return head
+
+        # Find the new tail (length - k - 1 steps from head)
+        new_tail = head
+        for _ in range(length - k - 1):
+            new_tail = new_tail.next
+
+        new_head = new_tail.next
+        new_tail.next = None
+        tail.next = head          # connect old tail to old head
+
+        return new_head
