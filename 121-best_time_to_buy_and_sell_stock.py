15ce05507bd481791bad95ad086e091d232e9fb0
Swee Ngee <110452211+Sparkysn@users.noreply.github.com>
2026-06-25 21:59:35 +0800

    121-solution brute force

---
+++ 121-best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,47 @@
+"""
+121. Best Time to Buy and Sell Stock
+Attempted
+Easy
+Topics
+conpanies icon
+Companies
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+ 
+
+Constraints:
+
+1 <= prices.length <= 105
+0 <= prices[i] <= 104
+ 
+"""
+
+#time: O(n^2)
+#space: O(n)
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = float('-inf')
+        for i in range(len(prices)):
+            for j in range(i+1,len(prices)):
+                profit = max(profit, prices[j] - prices[i])
+        return 0 if profit <= 0 else profit
+
+
