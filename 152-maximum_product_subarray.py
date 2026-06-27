ede8079793dc4c0d043a4413dd4ce630aa0d05af
Swee Ngee <110452211+Sparkysn@users.noreply.github.com>
2026-06-27 10:37:57 +0800

    152-brute force solution

---
+++ 152-maximum_product_subarray.py
@@ -0,0 +1,51 @@
+"""
+152. Maximum Product Subarray
+Attempted
+Medium
+Topics
+conpanies icon
+Companies
+Given an integer array nums, find a subarray that has the largest product, and return the product.
+
+The test cases are generated so that the answer will fit in a 32-bit integer.
+
+Note that the product of an array with a single element is the value of that element.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+Example 2:
+
+Input: nums = [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-10 <= nums[i] <= 10
+The product of any subarray of nums is guaranteed to fit in a 32-bit integer.
+"""
+
+
+#time: O(n^2)
+#space: O(1)
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        max_product = float('-inf')
+        if len(nums) == 1:
+            return nums[0]
+        for i in range(len(nums)):
+            product = nums[i]
+            max_product = max(max_product, product)
+            for j in range(i+1, len(nums)):
+                product*= nums[j]
+                max_product = max(max_product, product)
+        return max_product if max_product != float('-inf') else 0
+            
