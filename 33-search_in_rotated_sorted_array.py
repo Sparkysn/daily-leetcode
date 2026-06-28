74b2197eb8cd29422b1d20524ae631360f06494d
Swee Ngee <110452211+Sparkysn@users.noreply.github.com>
2026-06-28 15:36:39 +0800

    33-optimized solution
    Implemented a binary search algorithm to find the target in a rotated sorted array.

--- 33-search_in_rotated_sorted_array.py
+++ 33-search_in_rotated_sorted_array.py
@@ -104,8 +104,38 @@ class Solution:
         
         return shiftedBinarySearch(left, target)
 
+#time: O(logn)
+#space: O(1)
+# 1 pass binary search
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        left = 0
+        right = n - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            # nums[mid] >= nums[left] and not only nums[mid] > nums[left] because edge case [3,1], we need treat left as sorted
+            elif nums[mid] >= nums[left]:
+                if target >= nums[left] and target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if target <= nums[right] and target > nums[mid]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return -1
+                 
+
 
                  
+
+     
 
         
                  
