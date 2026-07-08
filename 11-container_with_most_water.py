41421215ac379603aa9a2517c8df0a3efe933d41
Swee Ngee <110452211+Sparkysn@users.noreply.github.com>
2026-07-08 21:49:14 +0800

    11-two pointer solution

--- 11-container_with_most_water.py
+++ 11-container_with_most_water.py
@@ -46,3 +46,22 @@ class Solution:
                 temp_max_area =  (r - l) * min(height[l],height[r]) 
                 max_area = max(temp_max_area,max_area)
         return max_area
+
+#time: O(n)
+#space: O(1)
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        max_area = 0
+        left = 0
+        right = len(height) - 1
+        while left < right:
+            min_height = min(height[left], height[right])
+            width = right - left
+            max_area = max(max_area, min_height*width)
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_area
