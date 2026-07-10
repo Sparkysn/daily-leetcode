f3cb5f2052c199c486c22be41d588a6ba3bd4cdf
Swee Ngee <110452211+Sparkysn@users.noreply.github.com>
2026-07-10 22:40:48 +0800

    125-space optimized using two pointer solution

--- 125-valid_palindrome.py
+++ 125-valid_palindrome.py
@@ -53,3 +53,46 @@ class Solution:
                 left += 1
                 right -= 1
         return True
+
+#time: O(n)
+#space: O(1)
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            if not s[left].isalnum():
+                left += 1
+                continue
+            if not s[right].isalnum():
+                right -= 1
+                continue
+            if s[left].lower() != s[right].lower():
+                return False
+            else:
+                left += 1
+                right -= 1
+        return True
+
+#time: O(n)
+#space: O(1)
+
+# alternative way to write
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+            
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+        return True
+
