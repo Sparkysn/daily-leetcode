779aaa25512f0d2b8464e0e0ef1fbd62bd3e655c
Swee Ngee <110452211+Sparkysn@users.noreply.github.com>
2026-07-22 21:30:59 +0800

    22-backtracking solution

--- 22-generate_parentheses.py
+++ 22-generate_parentheses.py
@@ -51,4 +51,27 @@ class Solution:
                     return False
                 left_count -= 1
         return left_count == 0
+
+# time: O(4ⁿ / n√n)
+# space: O(n)
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        answer = []
+
+        def backTracking(curr_string: str, left_count: int, right_count: int):
+            if len(curr_string) == 2 * n:
+                answer.append("".join(curr_string))
+                return
+            if left_count < n:
+                curr_string.append("(")
+                backTracking(curr_string, left_count + 1, right_count)
+                curr_string.pop()
+            if right_count < left_count:
+                curr_string.append(")")
+                backTracking(curr_string, left_count, right_count + 1)
+                curr_string.pop()
     
+        backTracking([], 0 ,0 )
+        return answer
