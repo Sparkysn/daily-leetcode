80bdb3dab378c4c6e057ab5a030c25c40076922e
Swee Ngee <110452211+Sparkysn@users.noreply.github.com>
2026-07-26 19:35:43 +0800

    17-backtrack solution

---
+++ 17-letter_combinations_of_a_phone_number.py
@@ -0,0 +1,63 @@
+"""
+17. Letter Combinations of a Phone Number
+Solved
+Medium
+Topics
+conpanies icon
+Companies
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+
+ 
+
+Example 1:
+
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+Example 2:
+
+Input: digits = "2"
+Output: ["a","b","c"]
+ 
+
+Constraints:
+
+1 <= digits.length <= 4
+digits[i] is a digit in the range ['2', '9'].
+"""
+
+# time: O(n*4^n)
+# space: O(n)
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        custom_keypad = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+
+        ans = []
+        n = len(digits)
+        def backTrack(index: int, curr: str):
+            if len(curr) == n:
+                ans.append("".join(curr))
+                return
+            letters = custom_keypad[digits[index]]
+            for letter in letters:
+                curr.append(letter)
+                backTrack(index + 1, curr)
+                curr.pop()
+        backTrack(0,[])
+        return ans
+
+
+
+
