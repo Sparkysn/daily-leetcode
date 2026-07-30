82856b3b3e63c607430cc6762062e9bc7467161d
Swee Ngee <110452211+Sparkysn@users.noreply.github.com>
2026-07-30 23:47:49 +0800

    74-binarysearch once

--- 74-search_a_2d_matrix.py
+++ 74-search_a_2d_matrix.py
@@ -74,3 +74,28 @@ class Solution:
         return False
 
     
+# time: O(logn)
+# space: O(1)
+
+# bs once
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix)
+        n = len(matrix[0])
+        left = 0
+        right = (m*n) - 1
+        while left <= right:
+            mid = left + (right - left) // 2 
+            row = mid // n
+            col = mid % n
+            if target == matrix[row][col]:
+                return True
+            elif target < matrix[row][col]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return False
+    
+
+
